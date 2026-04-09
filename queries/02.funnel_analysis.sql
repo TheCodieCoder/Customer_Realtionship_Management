edf012dd6bb28e188c8dd441471cbefed4857c55
Misbah Ur Rahman <156216586+TheCodieCoder@users.noreply.github.com>
2026-04-10 00:50:34 +0530

    Update 02.funnel_analysis.sql

--- queries/02.funnel_analysis.sql
+++ queries/02.funnel_analysis.sql
@@ -1,5 +1,52 @@
+-- 1 Sales Funnel Analysis (funnel by employee)
+-- Analyze the sales funnel: total leads, converted deals, won deals, and conversion percentages.
 
--- 1. Where are we losing leads BEFORE they become deals?(sales funnel)
+WITH lead_count AS (
+	SELECT 
+    assigned_to AS employeeId,
+    COUNT(*) AS lc
+    FROM Lead_
+    GROUP BY employeeId
+),
+deal_count AS (
+	SELECT 
+    owner_id AS employeeId,
+    COUNT(*) AS dc
+    FROM Deal
+    GROUP BY employeeId
+),
+won_deal_count AS (
+	SELECT 
+    owner_id AS employeeId,
+    COUNT(*) AS wdc
+    FROM Deal
+    WHERE stage = "won"
+    GROUP BY employeeId
+)
+
+SELECT 
+	l.employeeId,
+    COALESCE(l.lc, 0) AS leads,
+    COALESCE(d.dc, 0) AS deals,
+    COALESCE(wd.wdc, 0) AS won_deals,
+    
+    CASE
+		WHEN COALESCE(l.lc, 0) = 0 THEN 0
+        ELSE ROUND(COALESCE(d.dc, 0) * 100.0 / l.lc)
+	END AS deal_rate,
+    
+    CASE
+		WHEN COALESCE(d.dc, 0) = 0 THEN 0
+        ELSE ROUND(COALESCE(wd.wdc, 0) * 100.0 / d.dc)
+	END AS won_deal_rate
+    
+FROM lead_count l
+LEFT JOIN deal_count d
+ON l.employeeId = d.employeeId
+LEFT JOIN won_deal_count wd
+ON d.employeeId = wd.employeeId;    
+
+-- 2. Where are we losing leads BEFORE they become deals?(sales funnel)
 
 
 WITH lead_count AS (
@@ -34,7 +81,7 @@ ON l.employeeId = d.employeeId
 ORDER BY deal_rate ASC;
 
 
--- 2. Employees with below average performance(sales funnel)
+-- 3. Employees with below average performance(sales funnel)
 
 WITH lead_count AS (
 	SELECT
